243ed6f56d91ed935f4c1c1359f61f80a8fb895c
Faiz Mohammad Hafidza <faiz.hafidza@code.id>
2025-02-25 06:24:55 +0700

    initial commit

---
+++ src/components/ChatMessage.tsx
@@ -0,0 +1,33 @@
+import ReactMarkdown from "react-markdown";
+import { ThoughtMessage } from "./ThoughtMessage";
+
+interface ChatMessageProps {
+  role: "user" | "assistant";
+  content: string;
+  thought?: string;
+}
+
+export const ChatMessage = (props: ChatMessageProps) => {
+  const isAssistant = props.role === "assistant";
+  return (
+    <>
+      {!!props.thought && <ThoughtMessage thought={props.thought} />}
+      <div
+        className={`flex items-start gap-4 ${
+          isAssistant ? "flex-row" : "flex-row-reverse"
+        }`}
+      >
+        <div
+          className={`rounded-lg p-4 max-w-[80%] ${
+            isAssistant ? "bg-secondary" : "bg-primary text-primary-foreground"
+          }`}
+        >
+          <ReactMarkdown
+            className={isAssistant ? "prose dark:prose-invert" : ""}
+            children={props.content.trim()}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
